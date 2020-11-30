ffb53fc757c57b0c3f320d5de7e8d70981de4481
jv.lee@foxmail.com <jv.lee@foxmail.com>
2020-11-30 23:24:32 +0800

    Image.kt 实现序列化接口;

--- app/src/main/java/com/lee/imagetools/entity/Image.kt
+++ app/src/main/java/com/lee/imagetools/entity/Image.kt
@@ -2,14 +2,17 @@ package com.lee.imagetools.entity
 
 import android.content.ContentUris
 import android.net.Uri
+import android.os.Parcelable
 import android.provider.MediaStore
+import kotlinx.android.parcel.Parcelize
 
 /**
  * @author jv.lee
  * @date 2020/11/30
  * @description
  */
-data class Image(val id: Long,val name: String, val path: String,val timestamp:Long) {
+@Parcelize
+data class Image(val id: Long,val name: String, val path: String,val timestamp:Long) : Parcelable {
     fun getImageUri(): Uri {
         return ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
     }
