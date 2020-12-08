02ab33adbc4447c8688ad97d0322c9d488cce221
jv.lee <jv.lee@foxmail.com>
2020-12-08 14:52:25 +0800

    添加专辑 id转URI函数

--- select/src/main/java/com/imagetools/select/entity/Album.kt
+++ select/src/main/java/com/imagetools/select/entity/Album.kt
@@ -1,8 +1,16 @@
 package com.imagetools.select.entity
 
+import android.content.ContentUris
+import android.net.Uri
+import android.provider.MediaStore
+
 /**
  * @author jv.lee
  * @date 2020/11/30
  * @description
  */
-internal data class Album(val id: Long, val name: String, val cover: String)
+internal data class Album(val id: Long, val name: String, val cover: String){
+    fun getImageUri(): Uri {
+        return ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+    }
+}
