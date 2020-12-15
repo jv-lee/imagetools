fa01fde46e9c62cbb09ebb10dcf450f025216271
jv.lee <jv.lee@foxmail.com>
2020-12-15 19:31:04 +0800

    添加部分demo代码.

--- app/src/main/java/com/imagetools/app/ImageDetailsActivity.kt
+++ app/src/main/java/com/imagetools/app/ImageDetailsActivity.kt
@@ -3,6 +3,8 @@ package com.imagetools.app
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import com.imagetools.app.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_image.*
 
@@ -11,7 +13,8 @@ import kotlinx.android.synthetic.main.activity_image.*
  * @date 2020/12/14
  * @description
  */
-class ImageDetailsActivity : BaseActivity(R.layout.activity_image_details) {
+class ImageDetailsActivity : BaseActivity(R.layout.activity_image_details),
+    GestureDetector.OnGestureListener {
 
     companion object {
         const val TAG = "IMAGE_DETAILS"
@@ -21,10 +24,52 @@ class ImageDetailsActivity : BaseActivity(R.layout.activity_image_details) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val gd = GestureDetector(this, this)
 
         iv_image.setOnTouchListener { v, event ->
-            event.x
+            v.translationX = event.x
+            v.translationY = event.y
             true
+//            gd.onTouchEvent(event)
         }
     }
+
+    override fun onShowPress(e: MotionEvent?) {
+        Log.i(TAG, "onShowPress: ")
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        Log.i(TAG, "onSingleTapUp: ")
+        return true
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        Log.i(TAG, "onDown: ")
+        return true
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        Log.i(TAG, "onFling: ")
+        return true
+    }
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        Log.i(TAG, "onScroll: ")
+        return true
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+        Log.i(TAG, "onLongPress: ")
+    }
+
 }
