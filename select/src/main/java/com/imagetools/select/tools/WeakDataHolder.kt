5ac88a072a385687746382fa83b7fbc75287d615
jv.lee <jv.lee@foxmail.com>
2021-04-27 11:52:19 +0800

    解决intent数据过大问题.

--- select/src/main/java/com/imagetools/select/tools/WeakDataHolder.kt
+++ select/src/main/java/com/imagetools/select/tools/WeakDataHolder.kt
@@ -8,6 +8,7 @@ import java.lang.ref.WeakReference
  * @description 大量数据临时缓存（解决intent无法携带超过1MB数据跳转问题）
  */
 class WeakDataHolder {
+
     companion object {
         val instance by lazy { WeakDataHolder() }
     }
@@ -17,19 +18,19 @@ class WeakDataHolder {
     /**
      * 数据存储
      * @param key
-     * @param object
+     * @param data
      */
-    fun saveData(key: String, `object`: Any?) {
-        dataMap[key] = WeakReference(`object`)
+    fun <T> saveData(key: String, data: T?) {
+        dataMap[key] = WeakReference(data)
     }
 
     /**
      * 获取数据
      * @param id
-     * @return
+     * @return data
      */
     fun <T> getData(key: String): T? {
-        val weakReference: WeakReference<Any?> = dataMap[key] as WeakReference<Any?>
-        return weakReference.get() as T?
+        val weakReference: WeakReference<T?> = dataMap[key] as WeakReference<T?>
+        return weakReference.get()
     }
 }
